1cd296d73717db9166dce562f5b1b7c5e1e673d2
Piero Giusti <pierophp@gmail.com>
2020-01-27 14:26:45 -0300

    Fix background

--- src/pages/Cards.css.js
+++ src/pages/Cards.css.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(theme => ({
   cardContainer: {
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center center',
     height: 'calc(100vh - 56px)',
