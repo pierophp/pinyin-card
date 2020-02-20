3a43b2f1cfca8ff77b417ed791b2bd91355f03de
Piero Giusti <pierophp@gmail.com>
2020-02-20 15:13:54 -0300

    Refactor

--- src/pages/EnglishPortugueseIpa.css.ts
+++ src/pages/EnglishPortugueseIpa.css.ts
@@ -3,4 +3,9 @@ export default makeStyles(theme => ({
   container: {
     padding: '20px 20px',
   },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
 }));
--- src/pages/EnglishPortugueseIpa.tsx
+++ src/pages/EnglishPortugueseIpa.tsx
@@ -1,79 +1,114 @@
 import React from 'react';
 import useStyles from './EnglishPortugueseIpa.css';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
 import Consonants from '../components/ipa/Consonants';
+import SyllabicConsonants from '../components/ipa/SyllabicConsonants';
 import StrongVowels from '../components/ipa/StrongVowels';
 import WeakVowels from '../components/ipa/WeakVowels';
+import MuiExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import { withStyles } from '@material-ui/core/styles';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const ExpansionPanel = withStyles({
+  root: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:before': {
+      display: 'none',
+    },
+    '&$expanded': {
+      margin: 'auto',
+    },
+  },
+  expanded: {},
+})(MuiExpansionPanel);
+
+const ExpansionPanelSummary = withStyles({
+  root: {
+    backgroundColor: 'rgba(0, 0, 0, .03)',
+    borderBottom: '1px solid rgba(0, 0, 0, .125)',
+    marginBottom: -1,
+    minHeight: 56,
+    '&$expanded': {
+      minHeight: 56,
+    },
+  },
+  content: {
+    '&$expanded': {
+      margin: '12px 0',
+    },
+  },
+  expanded: {},
+})(MuiExpansionPanelSummary);
 
 const EnglishPortugueseIpa = () => {
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState('');
+
+  const handleChangeExpanded = (newExpanded: any) => {
+    setExpanded(newExpanded !== expanded ? newExpanded : false);
+  };
 
   return (
     <div className={classes.container}>
-      <Typography gutterBottom variant="h5" component="h2">
-        Consoantes
-      </Typography>
-      <Consonants />
+      <ExpansionPanel
+        square
+        expanded={expanded === 'consonants'}
+        onChange={() => handleChangeExpanded('consonants')}
+      >
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>Consoantes</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Consonants />]{' '}
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel
+        square
+        expanded={expanded === 'strongVowels'}
+        onChange={() => handleChangeExpanded('strongVowels')}
+      >
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>Vogais Fortes</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <StrongVowels />
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel
+        square
+        expanded={expanded === 'weakVowels'}
+        onChange={() => handleChangeExpanded('weakVowels')}
+      >
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>Vogais Fracas</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <WeakVowels />
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel
+        square
+        expanded={expanded === 'syllabicConsonants'}
+        onChange={() => handleChangeExpanded('syllabicConsonants')}
+      >
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>
+            Consoante silábica
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <SyllabicConsonants />
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
       <br />
       <br />
-      <Typography gutterBottom variant="h5" component="h2">
-        Vogais Fortes
-      </Typography>
-      <StrongVowels />
-      <br />
-      <br />
-      <Typography gutterBottom variant="h5" component="h2">
-        Vogais Fracas
-      </Typography>
-      <WeakVowels />
-      <br />
-      <br />
-      <Typography gutterBottom variant="h5" component="h2">
-        Consoante silábica
-      </Typography>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>IPA</TableCell>
-            <TableCell>Inglês</TableCell>
-            <TableCell>Português</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>əl</TableCell>
-            <TableCell>
-              bott<b>le</b>
-            </TableCell>
-            <TableCell>
-              Goog<b>le</b>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>ən</TableCell>
-            <TableCell>
-              butt<b>on</b>
-            </TableCell>
-            <TableCell>
-              arf<b>am</b>, órf<b>ão</b>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>əm</TableCell>
-            <TableCell>
-              rhyth<b>m</b>
-            </TableCell>
-            <TableCell>
-              arf<b>am</b>, órf<b>ão</b>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+      <Typography gutterBottom variant="h5" component="h2"></Typography>
       <b>Fontes:</b>{' '}
       <a href="https://en.wikipedia.org/wiki/Help:IPA/English" target="_blank">
         Inglês
