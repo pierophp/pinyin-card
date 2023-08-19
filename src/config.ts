b3db494cf300480039fe5d541799875343f5b85c
Piero Giusti <pierophp@gmail.com>
2023-08-19 10:46:36 -0300

    Vite

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,20 @@
 const config: any = {
   development: {
-    apiUrl: 'http://localhost:5000',
-    pinyinApiUrl: 'https://api.pinzi.org',
+    // apiUrl: 'http://localhost:5000',
+    // pinyinApiUrl: 'https://api.pinzi.org',
+    apiUrl: "https://cards-api.pinzi.org",
+    pinyinApiUrl: "https://api.pinzi.org",
   },
   production: {
-    apiUrl: 'https://cards-api.pinzi.org',
-    pinyinApiUrl: 'https://api.pinzi.org',
+    apiUrl: "https://cards-api.pinzi.org",
+    pinyinApiUrl: "https://api.pinzi.org",
   },
 };
 
 const env = process.env.NODE_ENV as any;
 
 if (!config[env]) {
-  throw new Error('Env not configured');
+  throw new Error("Env not configured");
 }
 
 export default config[env];
